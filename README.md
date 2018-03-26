c627470a191cd13d962d2a5aa53930b53dd16210
autcrock <mjt464@gmail.com>
2018-03-26 21:15:02 +1000

    Add Linux build script (probably other unices too, but only Linux tested).

--- README.md
+++ README.md
@@ -2,12 +2,25 @@
 
 This program uses a simple list based Dijkstra algorithm to determine distances between vertices in a graph.  Neither the algorithm nor the underlying data structures are optimised for large real-world data.  The program can handle multiple disjoint graphs in it's persistent database, and will return JSON indicating that two places are not connected if asked to determine the distance between two unconnected places.
 
+WINDOWS 10
+
 To build it on Windows, just pull the git sources, ensure you have the Haskell stack utility installed and on your path then, at a Powershell prompt, type:
 
          .\test\RunCommandLineTests.ps1
 
-This will build the program sd.exe, and run the tests.  It will also run a series of command line examples using sd.exe via stack exec (which adds considerable startup overheads, so each line will run pause considerably before executing).
+LINUX
+
+To build it on Linux, just pull the git sources, ensure you have the Haskell stack utility installed and on your path then, at a Bash prompt, type:
+
+         sh .\test\RunCommandLineTests.sh
+
+
+This will build the program sd (or on Windows sd.exe), and run the tests.  It will also run a series of command line examples using sd.exe via stack exec (which adds considerable startup overheads on Windows).
+
+
+MORE INFO
 
 To get more detailed information about the command line options, read the above script, and also run:
 
         stack exec -- sd --help
+
---
+++ test/RunCommandLineTests.sh
@@ -0,0 +1,97 @@
+echo "--------------------------------------------------------------------"
+echo ""
+echo "EXAMPLE USAGE OF sd.exe"
+echo "--------------------------------------------------------------------"
+echo ""
+echo "BUILDING sd.exe and RUNNING TESTS"
+stack test
+
+echo "--------------------------------------------------------------------"
+echo ""
+echo "SHOW sd.exe USAGE"
+stack exec -- sd --help
+
+echo "--------------------------------------------------------------------"
+echo ""
+echo "INITIALISE PERSISTENT MAP USING DATA PROVIDED WITH TEST PROBLEM"
+stack exec -- sd --initialise ./test/testmap2.json
+
+echo "--------------------------------------------------------------------"
+echo ""
+echo "SHORTEST PATH FROM A to B - see tests above for more comprehensive testing"
+stack exec -- sd --shortest '{"start": "A", "end": "B"}'
+echo ""
+echo "SHORTEST PATH FROM A to H"
+stack exec -- sd --shortest '{"start": "A", "end": "H"}'
+echo ""
+echo "SHORTEST PATH FROM A to C"
+stack exec -- sd --shortest '{"start": "A", "end": "C"}'
+echo ""
+echo "SHORTEST PATH FROM H to B"
+stack exec -- sd --shortest '{"start": "H", "end": "B"}'
+echo ""
+echo "SHORTEST PATH FROM H to C"
+stack exec -- sd --shortest '{"start": "H", "end": "C"}'
+echo ""
+echo "ADD destinations I and C to place H"
+stack exec -- sd --addplace '{"map":[{"place":"H", "destinations": [ {"to": "I", "distance": 100}, {"to": "C", "distance": 24}]}]}'
+echo ""
+echo "ADD new place U with destinations V and X"
+stack exec -- sd --addplace '{"map":[{"place":"U", "destinations": [ {"to": "V", "distance": 100}, {"to": "X", "distance": 32.5}]}]}'
+echo ""
+echo "SHORTEST PATH FROM H to I"
+stack exec -- sd --shortest '{"start": "H", "end": "I"}'
+echo ""
+echo "SHORTEST PATH FROM A to G"
+stack exec -- sd --shortest '{"start": "A", "end": "G"}'
+echo ""
+echo "DELETE PLACE H AND ROADS TO ITS NEIGHBOURS - ALTERS DISTANCE BETWEEN A and G."
+stack exec -- sd --xplace '{"map":[{"place":"H", "destinations": []}]}'
+echo ""
+echo "SHORTEST PATH FROM A to G"
+stack exec -- sd --shortest '{"start": "A", "end": "G"}'
+echo ""
+echo "SHORTEST PATH FROM A to C"
+stack exec -- sd --shortest '{"start": "A", "end": "C"}'
+echo ""
+echo "ADD road Z to U"
+stack exec -- sd --addroad '{"map":[{"place":"U", "destinations": [ {"to": "Z", "distance": 99.9999}]}]}'
+echo ""
+echo "SHORTEST PATH FROM Z to U"
+stack exec -- sd --shortest '{"start": "Z", "end": "U"}'
+echo ""
+echo "DELIBERATE FAILURE NEXT - THERE IS NO CONNECTION BETWEEN Z and A"
+stack exec -- sd --shortest '{"start": "Z", "end": "A"}'
+echo ""
+echo "ADD connection between U and C to allow A and Z to connect."
+stack exec -- sd --addroad '{"map":[{"place":"U", "destinations": [ {"to": "C", "distance": 123}]}]}'
+echo ""
+echo "THERE IS NOW A CONNECTION BETWEEN Z and A"
+stack exec -- sd --shortest '{"start": "Z", "end": "A"}'
+echo ""
+echo "REMOVE connection between U and C to stop A and Z connection."
+stack exec -- sd --xroad '{"map":[{"place":"U", "destinations": [ {"to": "C", "distance": 123}]}]}'
+echo ""
+echo "DELIBERATE FAILURE NEXT - THERE IS AGAIN NO CONNECTION BETWEEN Z and A"
+stack exec -- sd --shortest '{"start": "Z", "end": "A"}'
+echo ""
+echo "UPDATE DISTANCE BETWEEN U and Z to 10"
+stack exec -- sd --addroad '{"map":[{"place":"U", "destinations": [ {"to": "Z", "distance": 10}]}]}'
+echo ""
+echo "SHORTEST PATH FROM Z to U"
+stack exec -- sd --shortest '{"start": "Z", "end": "U"}'
+echo ""
+echo "ADD NEW DESTINATION H to C in database"
+stack exec -- sd --addroad '{"map":[{"place":"H", "destinations": [ {"to": "C", "distance": 2024.2}]}]}'
+echo ""
+echo "CHECK DISTANCE BETWEEN H and C"
+stack exec -- sd --shortest '{"start": "H", "end": "C"}'
+echo ""
+echo "CHECK DISTANCE BETWEEN C and H"
+stack exec -- sd --shortest '{"start": "C", "end": "H"}'
+echo ""
+echo "CHECK DISTANCE BETWEEN A and C again"
+stack exec -- sd --shortest '{"start": "A", "end": "C"}'
+echo ""
+echo "CLEAR THE PERSISTENT DATABASE"
+stack exec -- sd --clear
