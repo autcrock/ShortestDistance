4fc5313d1e44eb6a63287b161671a8da9b7a238e
Mike Thomas <mjt464@gmail.com>
2025-08-10 08:35:09 +1000

    Basic nix.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,23 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.haskell.lib.buildStackProject {
-  name = "my-haskell-test-project";
-  src = ./.;
+let
+#   threadscope = ...
+#   speedscope = ...
+in
+  stdenv.mkDerivation {
+    name = "Shortest-Path";
+    nativeBuildInputs = [
+      # Haskell tools
+      ghcid ghc
+
+      # Profiling tools
+#       threadscope
+#       speedscope
+    ];
+  }
+
+# pkgs.haskell.lib.buildStackProject {
+#   name = "my-haskell-test-project";
+#   src = ./.;
   # Optional: Add any additional build inputs (e.g., system libraries) here
   # buildInputs = [ pkgs.zlib ];
-}
+# }
